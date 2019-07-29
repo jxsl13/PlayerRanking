5700dc02050c5edc6a98169aa01a9180c9203d32
John Behm <jbehm@uos.de>
2019-07-29 18:58:48 +0200

    removed unneeded import

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,10 @@
 #include <iostream>
 #include "rankingserver.h"
 #include <thread>
-#include <chrono>
 
 
 int main(int argc, const char* argv[])
 {   
-    using namespace std::chrono_literals;
 
     std::string host = "127.0.0.1";
     size_t port = 6379;
